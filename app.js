7dd75198f01f59ce6740387405599139f848ad80
Akash A <aku151101@gmail.com>
2021-04-05 22:23:06 +0530

    Modified app.js

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const ejs = require("ejs");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json({
+//     extended: true
+// }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
